75c2d45920b8612e397ccba4625801f369008833
kaloun anis <kaloun.anis@gmail.com>
2018-04-12 00:53:20 +0100

    j'ai fait la partie de la connexion

--- app/src/main/java/com/android/pfe/activity/LoginActivity.java
+++ app/src/main/java/com/android/pfe/activity/LoginActivity.java
@@ -1,24 +1,40 @@
 package com.android.pfe.activity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.pfe.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText MailAdress;
     private EditText Password;
     private TextView RegisterLink;
     private Button LogButton;
+    private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, MenuActivity.class));
+            finish();
+        }
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
@@ -29,9 +45,49 @@ public class LoginActivity extends AppCompatActivity {
         LogButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent menu = new Intent(getApplicationContext(),MenuActivity.class);
-                startActivity(menu);
+                String email = MailAdress.getText().toString();
+                final String password = Password.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+              //  progressBar.setVisibility(View.VISIBLE);
+
+                //authenticate user
+
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                               // progressBar.setVisibility(View.GONE);
+                                if (!task.isSuccessful()) {
+                                    // there was an error
+                                    if (password.length() < 6) {
+                                        Toast.makeText(LoginActivity.this, "mot de passe trop court", Toast.LENGTH_LONG).show();
+
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "erreur de connexion", Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    Intent menu = new Intent(getApplicationContext(),MenuActivity.class);
+                                    startActivity(menu);
+                                    finish();
+                                }
+                            }
+                        });
             }
+
+
         });
         RegisterLink.setOnClickListener(new View.OnClickListener() {
             @Override
